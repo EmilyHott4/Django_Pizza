e40f70080a9c9dc4a647aed2ad557872c8898434
Michael-Tinker <michaelt19@protonmail.com>
2022-05-06 19:26:56 -0500

    no users, comments, images, or creative homepage

---
+++ DjangoShell.py
@@ -0,0 +1,26 @@
+import imp
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza
+
+pizzas = Pizza.objects.all()
+
+for p in pizzas:
+    print(p.id, '  ', p)
+
+
+
+p = Pizza.objects.get(id=1)
+
+print(p.id)
+print(p.pizza_name)
+
+
+
+toppings = p.topping_set.all()
+for t in toppings:
+    print(t.topping_name)
---
+++ pizzas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Pizza(models.Model):
+    pizza_name = models.CharField(max_length=75)
+
+    class Meta:
+        verbose_name_plural = 'pizzas'
+
+    def __str__(self):
+        return self.pizza_name
+
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping_name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return self.topping_name
